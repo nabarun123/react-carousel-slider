2e21524f2fa5333092fe7beaba6beae80cb60264
nabarun123 <97055112+nabarun123@users.noreply.github.com>
2022-05-21 21:27:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import CarouselSlider from "./CarouselSlider";
+export default function App() {
+  return (
+    <div className="App">
+      <CarouselSlider />
+    </div>
+  );
+}
